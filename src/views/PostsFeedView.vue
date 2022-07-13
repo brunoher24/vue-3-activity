9beedf600986b189900f339af0d4000481134f65
brunoher24 <brunoher24@gmail.com>
2022-07-13 14:13:10 +0200

    v-for (1/2) - posts implementation hard coded

--- src/views/PostsFeedView.vue
+++ src/views/PostsFeedView.vue
@@ -1,27 +1,63 @@
-<script setup>
+<script>
 import Post from "@/components/Post.vue";
+
+export default {
+  components: { Toto: Post },
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  mounted() {
+    let id = 1;
+    this.posts = [
+      {
+        id: ++id,
+        authorImg: "https://via.placeholder.com/600/771796",
+        authorFullname: "Jean Dupont",
+        title: "Super post 1",
+        content: "Super contenu 1",
+      },
+      {
+        id: ++id,
+        authorImg: "https://via.placeholder.com/600/771796",
+        authorFullname: "Pierre Durand",
+        title: "Super post 2",
+        content: "Super contenu 2",
+      },
+      {
+        id: ++id,
+        authorImg: "https://via.placeholder.com/600/771796",
+        authorFullname: "Emilie Dupont",
+        title: "Super post 3",
+        content: "Super contenu 3",
+      },
+    ];
+  },
+};
 </script>
 
 <template>
   <main>
     <h1>Dernières publications...</h1>
-    <Post>
-      <template #author-image>
-        <img src="https://via.placeholder.com/600/771796" alt="" />
-      </template>
 
-      <template #author-fullname>Bruno Héritier</template>
+    <ul>
+      <li v-for="post in posts" :key="post.id">
 
-      <template #post-title>
-        Super titre
-      </template>
+        {{ post.id}}
+        <Toto>
+          <template #author-image>
+            <img src="https://via.placeholder.com/600/771796" alt="" />
+          </template>
 
+          <template #author-fullname>Bruno Héritier</template>
 
-      
-      Est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae
-      ea dolores neque\nfugiat blanditiis voluptate porro
-      
-   
-    </Post>
+          <template #post-title> Super titre </template>
+
+          Est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae
+          ea dolores neque\nfugiat blanditiis voluptate porro
+        </Toto>
+      </li>
+    </ul>
   </main>
 </template>
